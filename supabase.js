17f81524afe796037270f43c33b3204f6a87b7f5
letien452272-bit <letien.452272@gmail.com>
2026-06-11 17:41:49 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://tnpdfcjvfkyzmzxvaorb.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_IOlxiWS-4pfUqR3Sbh0qyg_oZhe8FBT";
-
-var supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
